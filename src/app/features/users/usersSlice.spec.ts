0cdf5170dbb054a7882f071b663ae120f0e3f5cf
Álvaro Márquez <marquezalvaro13@gmail.com>
2021-07-24 14:03:04 +0200

    Add handing for users response

--- src/app/features/users/usersSlice.spec.ts
+++ src/app/features/users/usersSlice.spec.ts
@@ -1,3 +1,4 @@
+import { searchUsersAsync } from '../../thunks/searchUsers'
 import usersReducer from './usersSlice'
 
 describe('users reducer', () => {
@@ -5,8 +6,43 @@ describe('users reducer', () => {
     users: [],
     usersCount: 0,
   }
+  const response = {
+    total_count: 1,
+    incomplete_results: false,
+    items: [{
+      login: 'frank',
+      id: 20204556,
+      node_id: 'MDQ6VXNlcjIwMjA0NTU2',
+      avatar_url: 'https://avatars.githubusercontent.com/u/20204556?v=4',
+      gravatar_id: '',
+      url: 'https://api.github.com/users/frank',
+      html_url: 'https://github.com/frank',
+      followers_url: 'https://api.github.com/users/frank/followers',
+      following_url: 'https://api.github.com/users/frank/following{/ther_user',
+      gists_url: 'https://api.github.com/users/frank/gists{/gist_id}',
+      starred_url: 'https://api.github.com/users/frank/starred{/owner}{/repo}',
+      subscriptions_url: 'https://api.github.com/users/frank/subscriptions',
+      organizations_url: 'https://api.github.com/users/frank/orgs',
+      repos_url: 'https://api.github.com/users/frank/repos',
+      events_url: 'https://api.github.com/users/frank/events{/privacy}',
+      received_events_url: 'https://api.github.com/users/frank/eceived_events',
+      type: 'User',
+      site_admin: false,
+      score: 1.0
+    }]
+  }
   it('Should handle the inital state', () => {
     expect(usersReducer(undefined, { type: 'unknown' })).toEqual(initialState)
+  })
+
+  it('Should handle the search users response', () => {
+    expect(usersReducer(
+      initialState,
+      { type: searchUsersAsync.fulfilled.type, payload: response }
+    )).toEqual({
+      users: response.items,
+      usersCount: response.total_count
+    })
   })
 })
 
--- src/app/features/users/usersSlice.ts
+++ src/app/features/users/usersSlice.ts
@@ -1,6 +1,7 @@
 
 import { createSlice } from '@reduxjs/toolkit'
 import { IUsersState } from '../../models/UsersState.model'
+import { searchUsersAsync } from '../../thunks/searchUsers'
 
 const initialState: IUsersState = {
   users: [],
@@ -11,7 +12,13 @@ export const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
-
+  extraReducers: (builder) => {
+    builder
+      .addCase(searchUsersAsync.fulfilled, (state, action) => {
+        state.users = action.payload.items
+        state.usersCount = action.payload.total_count
+      })
+  }
 })
 
 
