54ceb2db262e196eae3f7a7850632f797580874f
Álvaro Márquez <marquezalvaro13@gmail.com>
2021-07-25 12:01:12 +0200

    Update jest config to ignore scss

--- jest.config.js
+++ jest.config.js
@@ -7,10 +7,12 @@ module.exports = {
     './jest.setup.js'
   ],
   moduleNameMapper: {
-    '\\.png$|.jpg$': '<rootDir>/src/__mocks__/fileMock.ts'
+    '\\.png$|.jpg$': '<rootDir>/src/__mocks__/fileMock.ts',
+    '^.+\\.(css|less|scss)$': 'identity-obj-proxy'
   },
   collectCoverage: true,
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest'
   }
 }
+
